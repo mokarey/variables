6d5cb40ab0478d87299e303cb8f01da6c0d36b8b
Valentin <107222626+mokarey@users.noreply.github.com>
2022-07-28 21:10:51 -0300

    subida

---
+++ laburo/java.js
@@ -0,0 +1,22 @@
+//debugger
+
+let edad = parseInt(prompt("Ingresar edad"));
+
+if (edad > 17) {
+    alert("Eres mayor de edad! Bienvenido.");
+}
+else { (edad < 18)
+alert("No eres mayor de edad.")
+}
+
+let apuesta = parseInt(prompt("Ingrese un monto"))
+for (let i = 1; i < apuesta; i++){
+    console.log ("Primera", i)
+}
+
+
+
+
+
+
+
